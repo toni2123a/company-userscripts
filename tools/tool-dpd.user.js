cdae8095a4eefc7b9fad58f8bd8968ae61e7673e
toni2123a <thiemo.schoeler@googlemail.com>
2025-10-01 07:30:00 +0200

    Create tool-dpd.user.js

---
+++ tools/tool-dpd.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Tool DPD
+// @namespace    https://github.com/deinUser/company-userscripts
+// @version      1.0.0
+// @description  kleines Test-Tool
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/deinUser/company-userscripts/main/tools/tool-abc.user.js
+// @downloadURL  https://raw.githubusercontent.com/deinUser/company-userscripts/main/tools/tool-abc.user.js
+// @grant        none
+// ==/UserScript==
+(function(){ alert("Tool DPD aktiv!"); })();
